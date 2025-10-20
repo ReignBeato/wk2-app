47644e5478c756524062d20508e41d414b35f364
ReignBeato <beato.reign2022@gmail.com>
2025-10-21 11:52:04 +1300

    SCRUM-1 something

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+function ListRender(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+  <li>{numbers}</li>
+  );
+  return (
+  <ul>{listItems}</ul>
+  );
+ }
+
+ function ListRenderUsingLoop(props) {
+  const numbers = props.numbers;
+  const listItems = [];
+  for( let i = 0; i < numbers.length; i++){
+    listItems.push(<li>{numbers[i]}</li>)
+  }
+  return (
+  <ul>{listItems}</ul>
+  );
+ }
+
+const nums = [1, 2, 8, 39]
+const names = ["John", "Sara", "Smith"];
+
 function App() {
+  let v1 = 90;
+  const [val, setVal] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ListRender numbers = {nums}/>
+      <ListRender numbers = {names}/>
+      <ListRenderUsingLoop numbers = {nums}/>
+      <ListRenderUsingLoop numbers = {names}/>
+      <Username/>
+      <h1> Current Value {val}</h1>
+      <button onClick={()=>setVal(val+1)}> Increment Value</button>
+      <h1></h1>
+      <button onClick={()=>setVal(val-1)}> Decrement Value</button>
+      {/* Conditional Rendering - will not always display only if condition is met */}
+      {val>=5 && <h3>Value is greater than or equal to 5</h3>}
+      {val>10 && <h3>Value is greater than 10</h3>}
+      {val>=10 && val<=15 && <h3>Value is between 10 and 15</h3>}
     </div>
   );
 }
-
 export default App;
+
+function Username() {
+  const [name, setName] = useState('');
+  
+  return (
+    <div>
+      <h1> Enter your name: </h1>
+      <input 
+      type="text"
+      value={name}
+      onChange={(e) => setName(e.target.value)}
+      />
+     <button onClick={()=> alert('Weclcome ${name}')}> OK</button>
+     {<h3> Welcome {name}</h3>}
+     </div>
+  );
+}
